c1abe50793549bab535aceb88147a9fefc3ae92b
Cartafina <camila.cartafina@kellogg.northwestern.edu>
2022-04-13 02:39:41 +0000

    finished lab 4

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -7,3 +7,10 @@
 -- +---------------+------------+-----------+-----------+
 
 
+SELECT teams.name, players.first_name, players.last_name, max(stats.home_runs)
+FROM stats
+INNER JOIN teams ON stats.team_id=teams.id
+INNER JOIN players ON stats.player_id=players.id
+WHERE teams.year=2019
+ORDER BY stats.home_runs DESC
+LIMIT 1
