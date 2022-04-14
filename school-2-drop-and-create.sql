fa688ae52218c0895d03e250cdf65ad86ca8a4bd
Cartafina <camila.cartafina@kellogg.northwestern.edu>
2022-04-14 00:18:25 +0000

    2 and 3

--- school-2-drop-and-create.sql
+++ school-2-drop-and-create.sql
@@ -1,3 +1,11 @@
 -- Drop the students table
-
+DROP TABLE students;
+-- Recreate it
+CREATE TABLE students (
+    id INTERGER,
+    first_name TEXT,
+    last_name TEXT,
+    phone TEXT,
+    email TEXT
+)
 -- Recreate it
